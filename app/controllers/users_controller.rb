2eb982beee1d00c2ac28375f5a9494bab0f3e29f
Noel Jane Bourg <noeljanebourg@gmail.com>
2020-03-27 14:35:55 -0700

    update user model and controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,30 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
+    if @users
+      render json: {
+        users: @users
+      }
+    else 
+      render json: {
+        status: 500,
+        errors: ['no users found']
+      }
+    end 
   end
 
   def show
+    @user = User.find(params[:id])
+    if @user
+      render json: {
+        user: @user
+      }
+    else
+      render json: {
+        status: 500, 
+        errors: ['user not found']
+      }
+    end
   end
 
   def new
@@ -12,7 +34,16 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to_root_path
+      login!
+      render json: {
+        status: :created,
+        user: @user
+      }
+    else
+      render json: {
+        status: 500,
+        errors: @user.errors.full_messages
+      }
     end
   end
 
