943b487246ac6ed333e47e89ba8e4ba92fc9fc7c
Noel Jane Bourg <noeljanebourg@gmail.com>
2020-04-16 17:40:59 -0700

    add controller methods for index, show, new, and create

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,14 +1,50 @@
 class MealsController < ApplicationController
   def index
+    @meals = Meal.all
+    if @meals
+      render json: {
+        meals: @meals
+      }
+    else
+      render json: {
+        status: 500,
+        errors: [ 'no meals found']
+      }
+    end
   end
 
   def show
+    @meal = Meal.find(params[:id])
+    if @meal
+      render json: {
+        meal: @meal
+      }
+    else
+      render json: {
+        status: 500,
+        errors: ['meal not found']
+      }
+    end
   end
 
   def new
+    @meal = Meal.new
   end
 
   def create
+    @meal = Meal.new(meal_params)
+    if @meal.save
+      render json: {
+        status: :created,
+        meal: @meal
+      }
+    else 
+      render json: {
+        status: 500, 
+        errors: @meal.errors.full_messages
+      }
+    end
+
   end
 
   def edit
@@ -19,4 +55,9 @@ class MealsController < ApplicationController
 
   def destroy
   end
+
+  private
+    def meal_params
+      params.require(:meal).permit(:name, :description, :date, :link_to_recipe, :user_id)
+    end
 end
