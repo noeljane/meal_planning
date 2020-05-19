119170652f6de1f609dc507150dd5789d05423fa
Noel Jane Bourg <noeljanebourg@gmail.com>
2020-05-18 17:34:02 -0700

    add gem and new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root 'users#index'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
-  get '/logged_in', to: 'sessions#is_logged_in?'
-  resources :users, only: [:create, :show, :index]
+  post '/login', to: "auth#login"
+  get "/auto_login", to: "auth#auto_login"
+  get "/user_is_authed", to "auth#user_is_authed"
+  # delete '/logout', to: 'sessions#destroy'
+  # get '/logged_in', to: 'sessions#is_logged_in?'
+  resources :users, only: [:create]
   resources :meals
   # root 'homepage#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
